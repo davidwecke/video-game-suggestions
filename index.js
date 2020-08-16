f926642078c4fca030cc3ce588a7f04e710ba08b
unknown <davidjohann1@gmail.com>
2020-08-16 12:18:16 -0400

    Fix DB Login

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const dburi = "mongodb+srv://Toblo:EiQ6bgLitt3L@cluster0.gxsld.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const dburi = process.env.ATLAS_URI;
 mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', () => {

64adb535a33de914138413aee0f112c74ebbec57
unknown <davidjohann1@gmail.com>
2020-08-16 13:06:24 -0400

    List Page Added

--- client/src/components/edit-suggestion.component.js
+++ client/src/components/edit-suggestion.component.js
@@ -1,11 +1,156 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class EditSuggestion extends Component {
+	constructor(props) {
+		super(props);
+
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+		this.onChangeGame = this.onChangeGame.bind(this);
+		this.onChangeDescription = this.onChangeDescription.bind(this);
+		this.onChangeVerified = this.onChangeVerified.bind(this);
+		this.onChangeSteamLink = this.onChangeSteamLink.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+
+		this.state = {
+			game: '',
+			description: '',
+			steam_link: '',
+			suggestor: '',
+			verified: false,
+			users: []
+		}
+	}
+
+	componentDidMount() {
+		axios.get('/api/suggestions'+this.props.match.params.id)
+			.then(response => {
+				this.setState({
+				game: response.data.game,
+				description: response.data.description,
+				steam_link: response.data.steam_link,
+				suggestor: response.data.suggestor,
+				verified: response.data.verified,
+				users: []
+				});
+			});
+		axios.get('/api/users/')
+			.then(response => {
+				if (response.data.length > 0) {
+					this.setState({
+						users: response.data.map(user => user.username),
+					})
+				}
+			})
+	}
+
+	onChangeUsername(event) {
+		this.setState({
+			username: event.target.value
+		});
+	}
+
+	onChangeGame(event) {
+		this.setState({
+			game: event.target.value
+		});
+	}
+
+	onChangeDescription(event) {
+		this.setState({
+			description: event.target.value
+		});
+	}
+
+	onChangeSteamLink(event) {
+		this.setState({
+			steam_link: event.target.value
+		});
+	}
+
+	onChangeVerified(event) {
+		this.setState({
+			verified: event.target.value
+		});
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+
+		const suggestion = {
+			suggestor: this.state.username,
+			game: this.state.game,
+			description: this.state.description,
+			steam_link: this.state.steam_link,
+			verified: this.state.verified
+		}
+
+		console.log(suggestion);
+
+		axios.post('/api/suggestions/update/'+this.props.match.params.id, suggestion)
+			.then(res => console.log(res.data));
+
+		window.location = '/';
+	}
+
 	render () {
 		return (
 			<div>
-				<p>You are on the Edit Suggestion component!</p>
+				<h3>Edit a Game Suggestion</h3>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Username: </label>
+						<select ref="userInput"
+							required
+							className="form-control"
+							value={this.state.username}
+							onChange={this.onChangeUsername}>
+							{
+								this.state.users.map(function(user) {
+									return <option 
+										key={user}
+										value={user}>{user}
+										</option>
+								})
+							}
+
+						</select>
+					</div>
+					<div className="form-group">
+						<label>Game Title</label>
+						<input type="text"
+							required
+							className="form-control"
+							value={this.state.game}
+							onChange={this.onChangeGame} />
+					</div>
+					<div className="form-group">
+						<label>Description</label>
+						<input type="text"
+							required
+							className="form-control"
+							value={this.state.description}
+							onChange={this.onChangeDescription} />
+					</div>
+					<div className="form-group">
+						<label>Steam Link</label>
+						<input type="text"
+							className="form-control"
+							value={this.state.steam_link}
+							onChange={this.onChangeSteamLink} />
+					</div>
+					<div className="form-group">
+						<label>Your Username</label>
+						<input type="text"
+							className="form-control"
+							value={this.state.username}
+							onChange={this.onChangeUsername} />
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Edit Game" className="btn btn-primary" />
+					</div>
+				</form>
 			</div>
-			);
+		);
 	}
 }
--- client/src/components/suggestion-list.component.js
+++ client/src/components/suggestion-list.component.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 import axios from 'axios';
+
+const Suggestion = props => (
+	<tr>
+		<td>{props.suggestion.game}</td>
+		<td>{props.suggestion.description}</td>
+		<td>{props.suggestion.steam_link}</td>
+		<td>{props.suggestion.username}</td>
+		<td>
+			<Link to={"/edit/"+props.suggestion._id}>Edit</Link> | <a href="#" onClick={() => {props.deleteSuggestion(props.suggestion._id)}}>Delete</a>
+		</td>
+	</tr>
+	)
 
 export default class SuggestionList extends Component {
 	constructor(props) {
 		super(props);
 
-		
+		this.deleteSuggestion = this.deleteSuggestion.bind(this);
+		this.suggestionList = this.suggestionList.bind(this);
 
 		this.state={suggestions: []};
 	}
@@ -21,10 +34,37 @@ export default class SuggestionList extends Component {
 			});
 	}
 
+	deleteSuggestion(id) {
+		axios.delete('/api/suggestions/'+id)
+			.then(res => console.log(res.data));
+		this.setState({
+			suggestions: this.state.suggestions.filter(el => el._id !== id)
+		});
+	}
+
+	suggestionList() {
+		return this.state.suggestions.map(currentSuggestion => {
+			return <Suggestion suggestion={currentSuggestion} deleteSuggestion={this.deleteSuggestion} key={currentSuggestion._id} />
+		});
+	}
+
 	render () {
 		return (
 			<div>
-				<p>You are on the Suggestion List component!</p>
+				<h3>Curent Game Suggestions</h3>
+				<table className="table">
+					<thead className="thead-light">
+						<tr>
+							<th>Game</th>
+							<th>Description</th>
+							<th>Steam Link</th>
+							<th>Suggested by</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.suggestionList() }
+					</tbody>
+				</table>
 			</div>
 			);
 	}
